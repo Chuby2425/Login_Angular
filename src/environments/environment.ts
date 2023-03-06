c4a5e70c922d4e1d1d0ba3b7ac858dbfa6ebaa7e
Chuby2425 <jeison_chuby0998@outlook.com>
2023-03-06 00:11:24 -0600

    cambio de info en firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,16 @@
+
+//Aqui se debe de poner, el firebase para que pueda funcionar con normalidad
+
+
 export const environment ={
   production: false,
   firebase: {
-    apiKey: "AIzaSyAMp0MG_68_crfM7qp4pB4FmotPMATUIYA",
-    authDomain: "angular-examen-29272.firebaseapp.com",
-    projectId: "angular-examen-29272",
-    storageBucket: "angular-examen-29272.appspot.com",
-    messagingSenderId: "1080784359785",
-    appId: "1:1080784359785:web:6e99266b42ecef51762df8"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
   };
 
